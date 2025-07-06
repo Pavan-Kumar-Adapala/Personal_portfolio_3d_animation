71bc7f5659becc877faadc021b400a0f4c76afe6
Pavan-Kumar-Adapala <pavankumar.adapala.msc@gmail.com>
2025-07-06 16:02:33 +0200

    build artifacts not loaded properly

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Personal_portfolio_3d_animation/',   // 👈 ADD THIS LINE
+  base: '/Personal_portfolio_3d_animation/',   // Base path for the project
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
